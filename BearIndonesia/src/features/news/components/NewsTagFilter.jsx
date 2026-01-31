4d74cf8e769a23eea51d4231fe13260b030aab56
owonye <wyw24@sookmyung.ac.kr>
2026-01-31 20:56:27 +0900

    feat: 경쟁사 태그 라벨에 티커/회사명 표기 추가
    회사명 태그를 (티커/회사명) 형식으로 표시
    필터 동작은 기존 태그 값 그대로 유지

--- BearIndonesia/src/features/news/components/NewsTagFilter.jsx
+++ BearIndonesia/src/features/news/components/NewsTagFilter.jsx
@@ -2,6 +2,63 @@
 
 import React from "react";
 
+const COMPANY_TICKER_LABELS = {
+  "PT Kalbe Farma Tbk": "KLBF/Kalbe Farma",
+  "Kalbe Farma": "KLBF/Kalbe Farma",
+  "PT Indofarma Tbk": "INAF/Indofarma",
+  "Indofarma": "INAF/Indofarma",
+  "PT Kimia Farma Tbk": "KAEF/Kimia Farma",
+  "Kimia Farma": "KAEF/Kimia Farma",
+  "PT Darya-Varia Laboratoria Tbk": "DVLA/Darya-Varia",
+  "Darya-Varia Laboratoria": "DVLA/Darya-Varia",
+  "PT Tempo Scan Pacific Tbk": "TSPC/Tempo Scan",
+  "Tempo Scan Pacific": "TSPC/Tempo Scan",
+  "PT Phapros Tbk": "PEHA/Phapros",
+  "Phapros": "PEHA/Phapros",
+  "PT Pyridam Farma Tbk": "PYFA/Pyridam Farma",
+  "Pyridam Farma": "PYFA/Pyridam Farma",
+  "PT Industri Jamu & Farmasi Sido Muncul Tbk": "SIDO/Sido Muncul",
+  "Sido Muncul": "SIDO/Sido Muncul",
+  "PT Merck Tbk": "MERK/Merck",
+  "Merck": "MERK/Merck",
+  "PT Medikaloka Hermina Tbk": "HEAL/Hermina",
+  "Medikaloka Hermina": "HEAL/Hermina",
+  "PT Mitra Keluarga Karyasehat Tbk": "MIKA/Mitra Keluarga",
+  "Mitra Keluarga Karyasehat": "MIKA/Mitra Keluarga",
+  "PT Prodia Widyahusada Tbk": "PRDA/Prodia",
+  "Prodia Widyahusada": "PRDA/Prodia",
+  "PT Royal Prima Tbk": "PRIM/Royal Prima",
+  "Royal Prima": "PRIM/Royal Prima",
+  "PT Sarana Meditama Metropolitan Tbk": "SAME/Sarana Meditama",
+  "Sarana Meditama Metropolitan": "SAME/Sarana Meditama",
+  "PT Siloam International Hospitals Tbk": "SILO/Siloam",
+  "Siloam International Hospitals": "SILO/Siloam",
+  "PT Sejahteraraya Anugrahjaya Tbk": "SRAJ/Sejahteraraya",
+  "Sejahteraraya Anugrahjaya": "SRAJ/Sejahteraraya",
+  "PT Diagnos Laboratorium Utama Tbk": "DGNS/Diagnos",
+  "Diagnos Laboratorium Utama": "DGNS/Diagnos",
+  "PT Bundamedik Tbk": "BMHS/Bundamedik",
+  "Bundamedik": "BMHS/Bundamedik",
+  "PT Famon Awal Bros Sedaya Tbk": "PRAY/Awal Bros",
+  "Famon Awal Bros Sedaya": "PRAY/Awal Bros",
+  "PT Diastika Biotekindo Tbk": "CHEK/Diastika Biotekindo",
+  "Diastika Biotekindo": "CHEK/Diastika Biotekindo",
+  "PT Haloni Jane Tbk": "HALO/Haloni Jane",
+  "Haloni Jane": "HALO/Haloni Jane",
+  "PT Itama Ranoraya Tbk": "IRRA/Itama Ranoraya",
+  "Itama Ranoraya": "IRRA/Itama Ranoraya",
+  "PT UBC Medical Indonesia Tbk": "LABS/UBC Medical",
+  "UBC Medical Indonesia": "LABS/UBC Medical",
+  "PT Hetzer Medical Indonesia Tbk": "MEDS/Hetzer Medical",
+  "Hetzer Medical Indonesia": "MEDS/Hetzer Medical",
+  "PT Multi Medika Internasional Tbk": "MMIX/Multi Medika",
+  "Multi Medika Internasional": "MMIX/Multi Medika",
+  "PT Jayamas Medica Industri Tbk": "OMED/Jayamas Medica",
+  "Jayamas Medica Industri": "OMED/Jayamas Medica",
+  "PT Maja Agung Latexindo Tbk": "SURI/Maja Agung",
+  "Maja Agung Latexindo": "SURI/Maja Agung",
+};
+
 const NewsTagFilter = ({ activeSection, currentSectionTags, activeTags, setActiveTags }) => {
   if (activeSection === "all" || currentSectionTags.length === 0) {
     return null;
@@ -52,7 +109,9 @@ const NewsTagFilter = ({ activeSection, currentSectionTags, activeTags, setActiv
           전체
         </button>
 
-        {currentSectionTags.map((tag) => (
+        {currentSectionTags.map((tag) => {
+          const label = COMPANY_TICKER_LABELS[tag] || tag;
+          return (
           <button
             key={tag}
             onClick={() => toggleTag(tag)}
@@ -68,9 +127,10 @@ const NewsTagFilter = ({ activeSection, currentSectionTags, activeTags, setActiv
               transition: "all 0.2s ease",
             }}
           >
-            {tag}
+            {label}
           </button>
-        ))}
+        );
+        })}
       </div>
     </div>
   );
